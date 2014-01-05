aa1d37fe332448a6217a88c9a7d3e776679d78a6
Laurence Liu <liuxy6@gmail.com>
2014-01-05 19:39:16 +0800

    add keep quiet

--- backup.sh
+++ backup.sh
@@ -10,15 +10,21 @@
 # 
 
 MYNAME=$(basename "$0")
-Version="0.1.1"
+Version="0.1.2"
 
 print_help() {
     cat << EOF
 $MYNAME $Version, backup my computer.
 Useage: $MYNAME [OPTION]
 
+Backup:
     -o, --output [/path/to/directory] output the file to the specified directory
+    -q, --quiet                       keep quiet
+    
+Check:
     -c, --check [/path/to/file]       check the file
+    
+Miscellaneous:
     -h, --help                        display this help and exit
     -V, --version                     print version information and exit
 
@@ -32,12 +38,12 @@ backup() {
         echo "Non root user. Please run as root."
         exit 1
     else
-        echo "Today is $(date +%Y-%m-%d). Backup begins."
-        tar -cpvzf $1/$(date +%Y-%m-%d).backup.tgz /etc /home/liu --exclude=.config/google-chrome --exclude=.config/google-chrome-unstable --exclude=Desktop --exclude=Documents --exclude=Downloads --exclude=Dropbox --exclude=.dropbox* --exclude=.local/share/Trash --exclude=Pictures --exclude=Templates --exclude=Videos --exclude=.cache --exclude=.purple --exclude=.thumbnails --exclude=/etc/fstab --exclude=/etc/hostname
-        comm -23 <(pacman -Qeq|sort) <(pacman -Qmq|sort) > $1/$(date +%Y-%m-%d).packagelist.txt
-        md5sum $1/$(date +%Y-%m-%d).backup.tgz $1/$(date +%Y-%m-%d).packagelist.txt > $1/$(date +%Y-%m-%d).md5sum.txt
-        chown liu $1/$(date +%Y-%m-%d).backup.tgz $1/$(date +%Y-%m-%d).packagelist.txt $1/$(date +%Y-%m-%d).md5sum.txt
-        chgrp liu $1/$(date +%Y-%m-%d).backup.tgz $1/$(date +%Y-%m-%d).packagelist.txt $1/$(date +%Y-%m-%d).md5sum.txt
+        echo "Today is $(date +%F). Backup begins."
+        tar -cpvzf $1/$(date +%F).backup.tgz /etc /home/liu --exclude={.bash_history,.config/google-chrome,.config/google-chrome-unstable,Desktop,Documents,Downloads,Dropbox,.dropbox*,.local/share/Trash,Pictures,.macromedia,Templates,Videos,.purple,.thumbnails,/etc/fstab,/etc/hostname,*cache*,*Cache*,*tmp*}
+        comm -23 <(pacman -Qeq|sort) <(pacman -Qmq|sort) > $1/$(date +%F).packagelist.txt
+        md5sum $1/$(date +%F).backup.tgz $1/$(date +%F).packagelist.txt > $1/$(date +%F).md5sum.txt
+        chown liu $1/$(date +%F).backup.tgz $1/$(date +%F).packagelist.txt $1/$(date +%F).md5sum.txt
+        chgrp liu $1/$(date +%F).backup.tgz $1/$(date +%F).packagelist.txt $1/$(date +%F).md5sum.txt
         echo "Done!"
     fi
 }
@@ -59,7 +65,17 @@ do
     case $1 in
         -o | --output )
             shift
-            backup $1
+            if [ "$2" = "-q" -o "$2" = "--quiet" ]
+            then
+                backup $1 > /dev/null
+                exit 0
+            else
+                backup $1
+                exit 0
+            fi
+            ;;
+        -q | --quiet )
+            backup . > /dev/null
             exit 0
             ;;
         -c | --check )
